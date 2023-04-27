31bb2f4ba29c24e46be26ca970d47248c8acca7c
Crea8Boss <kosilepalmeroluwole@gmail.com>
2023-04-27 18:31:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const nftcontainer = document.querySelector('.nft-container');
+const eye = document.querySelector('.eye');
+
+nftcontainer.addEventListener('click', showeye);
+
+function showeye (){
+    eye.style.display = 'grid';
+}
